dc3124d5cea5f1a2db1b05d88831b61e6e5566c6
wskirichenko <ws.kirichenko@gmail.com>
2020-10-07 18:00:25 +0300

    Added clear element method

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
   // console.log(table.description);
   let kolN = 10,        // Кол-во итераций
       kolCol = 6,       // Кол-во колонок в табл
-      numberFunc = 0,
-      countPushs = -1,
+      numberFunc = 0,   // Порядковый номер функции
+      countPushs = -1,  // Кол-во нажатий на кнопки с функциями
       count = 0,
-      k = 0,
+      cellNumber = 0,   // Номен ячейки в таблице для записи оченедного значения
       btn = document.getElementById("btn_start"),           // Кнопка вычислить
       btn_clear = document.getElementById("btn_clear"),     // Кнопка отчистить
       vFunc = document.getElementsByClassName("func-chouse-btn"),  // Кнопки выбора Функции
@@ -30,49 +30,43 @@ $(document).ready(function () {
     vibor(numberFunc, countPushs);
   };
 
+
   function vibor(numberFunc, countPushs) {
     tableMain.addHed(numberFunc, countPushs);
 
   };
 
-
-  function Vivod(col, k) {   // где k - порядковый номер ячейки табицы куда выводим
-    sell[k].innerHTML = k;
+  function Vivod(col, cellNumber) {   // где cellNumber - порядковый номер ячейки табицы куда выводим
+    sell[cellNumber].innerHTML = cellNumber;
   };
 
   // ---------------------------------------------------------------------------
   //          Кнопка Вычислить
   // ---------------------------------------------------------------------------
   btn.onclick = function() {
-
     for (var j = 0; j < kolN; j++) {
       tableMain.createTr();
       for (var i = 0; i < tableMain.numberColumns(numberFunc); i++) {
         tableMain.createTd(count);
-        Vivod(i,k);
-        k +=1;
+        Vivod(i, cellNumber);
+        cellNumber +=1;
       }
       count +=1;
-      console.log('count=',count, 'k=',k);
+      console.log('count=',count, 'cellNumber=', cellNumber);
     };
   };
+
 
   // ---------------------------------------------------------------------------
   //          Кнопка отчистить
   // ---------------------------------------------------------------------------
   btn_clear.onclick = function() {
-    let table1 = document.getElementsByTagName("table");
-    if (table1.length != 0) {
-      table1[0].remove();
-    }
-    let h2_1 = document.getElementsByTagName("h2");
-    if (h2_1.length != 0) {
-      h2_1[0].remove();
-    }
+    tableMain.clearElement(document.getElementsByTagName("table"));
+    tableMain.clearElement(document.getElementsByTagName("h2"));
     col_tables = 0;
-    countPushs = 0;
+    countPushs = -1;
     count = 0;
-    k = 0;
+    cellNumber = 0;
   };
 
 
--- src/modules/tableCreate.js
+++ src/modules/tableCreate.js
@@ -2,21 +2,6 @@
 // ---------------------------------------------------------------------------
 //          Модуль таблиц с метадими его создания
 // ---------------------------------------------------------------------------
-let tableHeader = {
-  0 : [   // Массив с заголоквами таблицы - 4 колонки
-    "Номер №", "Значения подходящих", "Значения модуля r",
-    "Значения аргумента fi"
-  ],
-  1 : [   // Массив с заголоквами таблицы - 6 колонок
-    "Номер №", "Значения подходящих", "Значения модуля r",
-    "Значения аргумента fi", "Погрешность модуля r", "Погрешность модуля fi"
-  ],
-  2 : [   // Массив с заголоквами таблицы - 7 колонок
-    "Номер №", "Значения подходящих 2", "Значения экспоненты", "Значения модуля r",
-    "Значения аргумента fi", "Погрешность модуля r", "Погрешность модуля fi"
-  ]
-}
-
 let tableHeader2 = {
   0 : { // Массив с заголоквами таблицы - 4 колонки
     title : "Tаблица - 4 колонки",
@@ -43,7 +28,7 @@ let tableHeader2 = {
 tableMain = {
   description: "Create Table",
   numberColumns(type_table) {
-    return tableHeader[type_table].length;
+    return tableHeader2[type_table].headers.length;
   },
 
   // Метод создания заголовков для таблицы
@@ -82,28 +67,25 @@ tableMain = {
     col_tables = col_tables+number_of_columns;
   },
 
-  /*addHed(type_table) {
-    number_of_columns = tableHeader[type_table].length; // Вычисляем количество колонок в заголовоке
-    tr = document.createElement('tr');    // Создаём элемент tr
-    thead.appendChild(tr);                // Добавляем внутрь таблицы новый tr
-
-    for (var i = 0; i < number_of_columns; i++) {         // Выводим заголовки таблицы начиная от 0 до количество колонок
-      th = document.createElement('th');  // Создаём элемент th
-      tr.appendChild(th);                 // Добавляем внутрь tr новый th
-      th.className = 't-hed';             // Присваеваем th класс
-      $(".t-hed")[i].innerHTML = tableHeader[type_table][i]; // Добавляем заголовок из массива tableHeader
-    }
-  },*/
   // Метод создания строки в main_tabl
   createTr() {
     tr = document.createElement('tr');        // Создаём элемент tr
     tr.className = 'tabl';                    // Присваеваем ему класс tabl
     tbody.appendChild(tr);                    // Добавляем внутрь (tbody className="tbody") новый tr
   },
+
   // Метод создания ячейки в строке main_tabl
   createTd(n) {
     td = document.createElement('td');  // Создаём элемент td
     td.className = 's1';                // Присваеваем ему класс
     $(".tabl")[n].appendChild(td);      // Добавляем внутрь строки (tr) новую ячейку (td)
+  },
+
+  // Метод удаления заданного элемента
+  clearElement(element) {
+    let elementLength = element.length;
+    for (let i = elementLength-1; i > -1; i--) {
+      element[i].remove();
+    }
   }
 }
