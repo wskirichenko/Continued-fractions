087440f896b905c449b961667cbc883ef8865d90
wskirichenko <ws.kirichenko@gmail.com>
2020-10-07 16:57:07 +0300

    Added dynamic table creation

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,39 @@
 "use strict";
-
+ let col_tables = 0;
 $(document).ready(function () {
   // console.log(table.description);
   let kolN = 10,        // Кол-во итераций
       kolCol = 6,       // Кол-во колонок в табл
       numberFunc = 0,
+      countPushs = -1,
+      count = 0,
+      k = 0,
       btn = document.getElementById("btn_start"),           // Кнопка вычислить
       btn_clear = document.getElementById("btn_clear"),     // Кнопка отчистить
-      vFunc = document.getElementsByClassName("func-chouse-btn"),  // Кнопка выбора Функции
+      vFunc = document.getElementsByClassName("func-chouse-btn"),  // Кнопки выбора Функции
       sell = document.getElementsByClassName('s1');         // Ячейка таблицы
   // ---------------------------------------------------------------------------
   vFunc[0].onclick = (e) => {
     numberFunc = e.currentTarget.getAttribute('value');
     console.log('numberFunc=', numberFunc);
-    vibor(numberFunc);
-  }
+    countPushs = countPushs + 1;
+    vibor(numberFunc, countPushs);
+  };
   vFunc[1].onclick = (e) => {
     numberFunc = e.currentTarget.getAttribute('value');
-    console.log('numberFunc=', numberFunc);
-    vibor(numberFunc);
-  }
+    countPushs = countPushs + 1;
+    vibor(numberFunc, countPushs);
+  };
+  vFunc[2].onclick = (e) => {
+    numberFunc = e.currentTarget.getAttribute('value');
+    countPushs = countPushs + 1;
+    vibor(numberFunc, countPushs);
+  };
 
-  function vibor(numberFunc) {
-    tableMain.addHed(numberFunc);
+  function vibor(numberFunc, countPushs) {
+    tableMain.addHed(numberFunc, countPushs);
 
-  }
+  };
 
 
   function Vivod(col, k) {   // где k - порядковый номер ячейки табицы куда выводим
@@ -35,25 +44,36 @@ $(document).ready(function () {
   //          Кнопка Вычислить
   // ---------------------------------------------------------------------------
   btn.onclick = function() {
-    let count = 0,
-        k = 0;
+
     for (var j = 0; j < kolN; j++) {
       tableMain.createTr();
-      for (var i = 0; i < kolCol; i++) {
+      for (var i = 0; i < tableMain.numberColumns(numberFunc); i++) {
         tableMain.createTd(count);
         Vivod(i,k);
         k +=1;
       }
       count +=1;
+      console.log('count=',count, 'k=',k);
     };
-  }
+  };
 
   // ---------------------------------------------------------------------------
   //          Кнопка отчистить
   // ---------------------------------------------------------------------------
   btn_clear.onclick = function() {
-
-  }
+    let table1 = document.getElementsByTagName("table");
+    if (table1.length != 0) {
+      table1[0].remove();
+    }
+    let h2_1 = document.getElementsByTagName("h2");
+    if (h2_1.length != 0) {
+      h2_1[0].remove();
+    }
+    col_tables = 0;
+    countPushs = 0;
+    count = 0;
+    k = 0;
+  };
 
 
 
--- src/modules/tableCreate.js
+++ src/modules/tableCreate.js
@@ -17,26 +17,88 @@ let tableHeader = {
   ]
 }
 
+let tableHeader2 = {
+  0 : { // Массив с заголоквами таблицы - 4 колонки
+    title : "Tаблица - 4 колонки",
+    headers : [
+      "Номер №", "Значения подходящих", "Значения модуля r",
+      "Значения аргумента fi"
+    ]
+  },
+  1 : { // Массив с заголоквами таблицы - 6 колонки
+    title : "Tаблица - 6 колонки",
+    headers : [
+      "Номер №", "Значения подходящих", "Значения модуля r",
+      "Значения аргумента fi", "Погрешность модуля r", "Погрешность модуля fi"
+    ]
+  },
+  2 : { // Массив с заголоквами таблицы - 7 колонки
+    title : "Tаблица - 7 колонки",
+    headers : [
+      "Номер №", "Значения подходящих 2", "Значения экспоненты", "Значения модуля r",
+      "Значения аргумента fi", "Погрешность модуля r", "Погрешность модуля fi"
+    ]
+  }
+}
 tableMain = {
   description: "Create Table",
+  numberColumns(type_table) {
+    return tableHeader[type_table].length;
+  },
+
   // Метод создания заголовков для таблицы
-  addHed(title_number) {
+  addHed(type_table, i) {             // где i - номер текущей таблицы (countPushs)
+    number_of_columns = tableHeader2[type_table].headers.length; // Вычисляем количество колонок в заголовоке
+    h2 = document.createElement('h2');        // Создаём элемент h2
+    main_panel.appendChild(h2);               // Добавляем внутрь 'main_panel' новый h2
+    h2.className = 'h2-table';                // Присваеваем h2 класс
+    $(".h2-table")[i].innerHTML = tableHeader2[type_table].title; // Добавляем title из массива tableHeader
+
+    table = document.createElement('table');  // Создаём элемент table
+    main_panel.appendChild(table);            // Добавляем внутрь 'main_panel' новый table
+    table.setAttribute("id", `table_${i}`);   // Добавляем id для table
+    table.className = 'table-main';           // Присваеваем table класс table-main
+
+    thead = document.createElement('thead');  // Создаём элемент thead
+    $(".table-main")[i].appendChild(thead);   // Добавляем внутрь 'table-main' новый thead
+    thead.setAttribute("id", `thead_${i}`);   // Добавляем id для thead
+    thead.className = 'thead';                // Присваеваем thead класс
+
+    tr = document.createElement('tr');        // Создаём элемент tr
+    $(".thead")[i].appendChild(tr);           // Добавляем внутрь таблицы новый tr
+
+    for (var j = 0; j < number_of_columns; j++) {  // Выводим заголовки таблицы начиная от 0 до количество колонок
+      th = document.createElement('th');      // Создаём элемент th
+      tr.appendChild(th);                     // Добавляем внутрь tr новый th
+      th.className = 'th-hed';                // Присваеваем th класс
+      $(".th-hed")[col_tables+j].innerHTML = tableHeader2[type_table].headers[j]; // Добавляем заголовок из массива tableHeader2.headers
+    }
+
+    tbody = document.createElement('tbody');  // Создаём элемент tbody
+    tbody.setAttribute("id", `tbody_${i}`);   // Добавляем id для tbody
+    tbody.className = 'tbody';                // Присваеваем tbody класс
+    $(".table-main")[i].appendChild(tbody);   // Добавляем внутрь 'table-main' новый tbody
+
+    col_tables = col_tables+number_of_columns;
+  },
+
+  /*addHed(type_table) {
+    number_of_columns = tableHeader[type_table].length; // Вычисляем количество колонок в заголовоке
     tr = document.createElement('tr');    // Создаём элемент tr
     thead.appendChild(tr);                // Добавляем внутрь таблицы новый tr
 
-    number_of_columns = tableHeader[title_number].length; // Вычисляем количество колонок в заголовоке
     for (var i = 0; i < number_of_columns; i++) {         // Выводим заголовки таблицы начиная от 0 до количество колонок
       th = document.createElement('th');  // Создаём элемент th
       tr.appendChild(th);                 // Добавляем внутрь tr новый th
       th.className = 't-hed';             // Присваеваем th класс
-      $(".t-hed")[i].innerHTML = tableHeader[title_number][i]; // Добавляем заголовок из массива tableHeader
+      $(".t-hed")[i].innerHTML = tableHeader[type_table][i]; // Добавляем заголовок из массива tableHeader
     }
-  },
+  },*/
   // Метод создания строки в main_tabl
   createTr() {
-    tr = document.createElement('tr');  // Создаём элемент tr
-    tr.className = 'tabl';              // Присваеваем ему класс
-    main_tabl.appendChild(tr);          // Добавляем внутрь (tbody className="main_tabl") новый tr
+    tr = document.createElement('tr');        // Создаём элемент tr
+    tr.className = 'tabl';                    // Присваеваем ему класс tabl
+    tbody.appendChild(tr);                    // Добавляем внутрь (tbody className="tbody") новый tr
   },
   // Метод создания ячейки в строке main_tabl
   createTd(n) {
